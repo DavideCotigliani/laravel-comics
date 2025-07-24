5ed52c81ff8e64234de8be1dc554709c60f9900e
DavideCotigliani <davide.cotigliani@gmail.com>
2025-07-24 15:29:43 +0200

    [UPD] Added all the comics to the page, adding some css

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,5 +1,23 @@
 @extends("layout.layout-principale")
+
+@php
+    $comics = config("comics");
+@endphp
 @section('contenuto')
 <h2>Tutti i fumetti</h2>
     <a href="{{route("home")}}">Torna alla homepage</a>
+    <div class="container">
+        <div class="row">
+            @foreach ($comics as $comic)
+            <div class="col">
+                <ul>
+                    <li>{{$comic["title"]}}</li>
+                    <li>   {{$comic["description"]}}</li>
+                    <li>{{$comic["price"]}}</li>
+
+                </ul>
+            </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
 @extends('layout.layout-principale')
 
+
 @section('contenuto')
 <h1>La mia homepage</h1>
 <a href="{{route("comics")}}">Visita la pagina dei fumetti</a>
